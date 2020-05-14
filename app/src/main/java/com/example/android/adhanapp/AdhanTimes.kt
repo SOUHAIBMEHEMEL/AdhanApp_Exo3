f0619e3a56a08f768fa8190f586f8a2394d09c55
unknown <fs_mehemel@esi.dz>
2020-05-14 07:54:18 +0100

    first commit

--- app/src/main/java/com/example/android/adhanapp/AdhanTimes.kt
+++ app/src/main/java/com/example/android/adhanapp/AdhanTimes.kt
@@ -2,11 +2,11 @@ package com.example.android.adhanapp
 
 class AdhanTimes {
 
-    var  Fajr:Int = ((5 * 60)+1) * 60
-    var  Dohr:Int = ((5 * 60)+4) * 60
-    var  Asr:Int = ((2 * 60)+30) * 60
-    var  Maghrib:Int = ((4 * 60)+14) * 60
-    var  Isha:Int = ((1 * 60)+47) * 60
+    var  Fajr:Int = ((4 * 60)+1) * 60
+    var  Dohr:Int = ((12 * 60)+45) * 60
+    var  Asr:Int = ((16 * 60)+34) * 60
+    var  Maghrib:Int = ((19 * 60)+50) * 60
+    var  Isha:Int = ((21 * 60)+34) * 60
 
     constructor(){}
 }
